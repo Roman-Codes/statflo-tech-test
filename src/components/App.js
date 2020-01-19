21e32d0733cf6ae285e8895991a1f5e7db494180
Roman <gobkins@gmail.com>
2020-01-19 14:31:03 -0500

    card flip implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/App.scss';
 import { connect } from 'react-redux';
+import ReactCardFlip from 'react-card-flip';
 
 import Form from './Form';
 import CardFront from './CardFront';
 import CardBack from './CardBack';
-import Ghost from './Ghost';
 
-const App = (props) =>{
+const App = ({ cvvSelected }) =>{
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = (arg) => {
+    arg === 'cvv' ? setFlipped(true) : setFlipped (false);
+  }
+
+  useEffect(()=>{
+    handleFlip(cvvSelected);
+  }, [cvvSelected]);
+
+
   return (
     <div className="App">
-      {props.cvvSelected === 'cvv' ? <CardBack /> : <CardFront />}
+      <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+        <CardFront key="front"/>
+        <CardBack key="back"/>
+      </ReactCardFlip>
       <Form />
     </div>
   );
